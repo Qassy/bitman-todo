6f627988b2d6752b6b735364c81fff77bd6cb358
Qas <mcqas05@gmail.com>
2020-10-31 19:48:46 -0700

    bug fixing, added support for configs

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ const mysql = require('mysql');
 const moment = require('moment-timezone');
 const config = require('./config.json');
 const pjson = require('./package.json');
-const timerShort = '⌛';
 moment().tz('America/Vancouver').format();
 moment.tz.setDefault('America/Vancouver');
 
@@ -21,23 +20,48 @@ async function pullQuery() {
 
 		const sqlC = 'SELECT * FROM classes';
 		const sqlA = 'SELECT * FROM assignments ORDER BY dueDate ASC';
+		const sqlS = 'SELECT * FROM config';
 
-		con.query(sqlC, function(err, resultC, fields) {
+		con.query(sqlS, function(err, resultS, fields) {
 			if (err) throw err;
-			const classes = resultC;
+			const configs = resultS;
 
-			con.query(sqlA, function(err, resultA, fields) {
+			con.query(sqlC, function(err, resultC, fields) {
 				if (err) throw err;
-				const assignments = resultA;
+				const classes = resultC;
 
-				updateMessage(classes, assignments);
-				con.end();
+				con.query(sqlA, function(err, resultA, fields) {
+					if (err) throw err;
+					const assignments = resultA;
+
+					updateMessage(classes, assignments);
+					updateBot(configs);
+					con.end();
+				});
 			});
 		});
 	});
 }
 
 const client = new Discord.Client();
+
+async function updateBot(configs) {
+	Object.keys(configs).forEach(function(keyConf) {
+		const rowConf = configs[keyConf];
+
+		switch (rowConf.variable) {
+			case 'motd':
+				client.user.setActivity(rowConf.value, { type: configs[1].value });
+				break;
+			case 'motdType':
+				// taken care in the "motd" case
+				break;
+			default:
+				console.log('Could not find function for setting ' + rowConf.variable);
+		}
+
+	});
+}
 
 async function updateMessage(classes, assignments) {
 	var embedString = '{"color": ' + Math.floor(Math.random() * 16777215) + ', "footer": {"text": "To-do list updated: ' + moment().format('MMM-Do h:mma') + '  •  BITMAN Task Manager: v' + pjson.version + '"}, "fields": [{"name": "> :arrow_down:  **Past Homework**  :arrow_down: ", "value": "\u200B"}]}';
@@ -49,8 +73,8 @@ async function updateMessage(classes, assignments) {
 		Object.keys(assignments).forEach(function(keyAsn) {
 			const rowAsn = assignments[keyAsn];
 			if (rowAsn.classID == rowCl.classID) {
-				if (moment(rowAsn.dueDate).isBefore() && moment(rowAsn.dueDate).diff(moment(), 'weeks') < 2) {
-					assignmentsLateString += '- ' + rowAsn.assignmentName + ' `' + moment(rowAsn.dueDate).format('MMM-Do h:mma') + ' (' + moment(rowAsn.dueDate, 'YYY-MM-DD hh:mm:ss').fromNow() + ')`\n';
+				if (moment(rowAsn.dueDate).isBefore() && moment().diff(moment(rowAsn.dueDate), 'days') <= 14) {
+					assignmentsLateString += '- ' + rowAsn.assignmentName + ' ` ' + moment(rowAsn.dueDate).format('MMM-Do h:mma') + ' (' + moment(rowAsn.dueDate, 'YYY-MM-DD hh:mm:ss').fromNow() + ') `\n';
 				}
 			}
 		});
@@ -79,8 +103,39 @@ async function updateMessage(classes, assignments) {
 		Object.keys(assignments).forEach(function(keyAsn) {
 			const rowAsn = assignments[keyAsn];
 			if (rowAsn.classID == rowCl.classID) {
-				if (moment(rowAsn.dueDate).isAfter()) {
-					assignmentsString += '- ' + rowAsn.assignmentName + '  ` ' + ((rowAsn.isRecurring == 0) ? moment(rowAsn.dueDate).format('MMM-Do h:mma') + ' (' + moment(rowAsn.dueDate, 'YYY-MM-DD hh:mm:ss').fromNow() + ') ' + ((moment(rowAsn.dueDate).diff(moment(), 'days') < 2) ? timerShort : '') : 'Weekly (' + moment().day(moment(rowAsn.dueDate).day()).fromNow() + ') ' + ((moment().day(moment(rowAsn.dueDate).day()).diff(moment(), 'days') < 2) ? timerShort : '')) + '`\n';
+				if (moment(rowAsn.dueDate).isAfter() && moment(rowAsn.showDate).isBefore()) {
+
+					var assignmentDueDate = '';
+					var assignmentDueDateDiff = moment(rowAsn.dueDate).diff(moment(), 'hours');
+					var assignmentDueDateDiffFormat = '';
+					var dueTimer = '';
+
+					if (rowAsn.isRecurring == 0) {
+						assignmentDueDate = moment(rowAsn.dueDate).format('MMM-Do h:mma');
+						assignmentDueDateDiffFormat = moment(rowAsn.dueDate, 'YYY - MM - DD hh: mm: ss ').fromNow();
+					} else {
+						assignmentDueDate = 'Weekly';
+
+						var assignmentDueDateDay = moment().day(moment(rowAsn.dueDate).day());
+
+						if (moment() <= assignmentDueDateDay) {
+							assignmentDueDateDiffFormat = moment(assignmentDueDateDay).fromNow();
+							// a date in the current week
+						} else {
+							assignmentDueDateDiffFormat = moment(assignmentDueDateDay).add(1, 'weeks').fromNow();
+							// a date in the previous week
+						}
+					}
+
+					if (assignmentDueDateDiff <= 96) {
+						dueTimer = '⌛ ';
+					} else {
+						dueTimer = '';
+					}
+
+					assignmentsString += '- ' + rowAsn.assignmentName + ' ` ' + assignmentDueDate + ' (' + assignmentDueDateDiffFormat + ') ' + dueTimer + '`\n';
+
+					// assignmentsString += '- ' + rowAsn.assignmentName + '  `' + ((rowAsn.isRecurring == 0) ? moment(rowAsn.dueDate).format('MMM - Do h: mma ') + ' (' + moment(rowAsn.dueDate, 'YYY - MM - DD hh: mm: ss ').fromNow() + ')' + ((moment(rowAsn.dueDate).diff(moment(), 'days ') < 2) ? timerShort : '') : 'Weekly(' + moment().day(moment(rowAsn.dueDate).day()).fromNow() + ')' + ((moment().day(moment(rowAsn.dueDate).day()).diff(moment(), 'days ') < 2) ? timerShort : '')) + '`\n';
 				}
 			}
 		});
@@ -109,7 +164,6 @@ async function updateMessage(classes, assignments) {
 
 client.once('ready', () => {
 	console.log('Ready!');
-	client.user.setActivity('garysucks.ca', { type: 'WATCHING' });
 	//const channel = client.channels.cache.get('754007715335897119');
 	//channel.send('beep boop task list goes here');
 	pullQuery();
