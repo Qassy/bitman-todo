49b6d71c1edf27991b06f9275445685ffd3cf475
Qas <mcqas05@gmail.com>
2021-09-12 17:44:10 -0600

    added schedule

--- index.js
+++ index.js
@@ -3,9 +3,10 @@ const mysql = require('mysql');
 const moment = require('moment-timezone');
 const config = require('./config.json');
 const pjson = require('./package.json');
-var tz = moment.tz.guess();
+var tz = 'America/Edmonton';
 moment().tz(tz).format();
 moment.tz.setDefault(tz);
+
 
 var showPastAssignments = 'off';
 var showFutureAssignments = 'off';
@@ -13,8 +14,11 @@ var showLegend = 'off';
 var showCustom = 'off';
 var customHeader = '\u200B';
 var customString = '\u200B';
+var showShedule = 'on';
 
-async function pullQuery() {
+var embedHeader = '{"color": ' + Math.floor(Math.random() * 16777215) + ', "footer": {"text": "Updated: ' + moment().format('MMM-Do h:mma') + '  •  BAIST BOT: v' + pjson.version + '"}, "fields": []}';
+
+function pullQuery() {
 	let con = mysql.createConnection({
 		host: config.sqlhost,
 		user: config.sqluser,
@@ -29,24 +33,35 @@ async function pullQuery() {
 		const sqlC = 'SELECT * FROM classes WHERE `classStart` < CURRENT_TIMESTAMP AND `classEnd` > CURRENT_TIMESTAMP';
 		const sqlA = 'SELECT * FROM assignments ORDER BY dueDate ASC';
 		const sqlS = 'SELECT * FROM config';
+		const sqlCS = 'SELECT * FROM `class_schedule`';
+
+		var configs = '';
+		var classSchedule = '';
+		var classes = '';
+		var assignments = '';
 
 		con.query(sqlS, function(err, resultS, fields) {
 			if (err) throw err;
-			const configs = resultS;
+			configs = resultS;
+			updateBot(configs);
+		});
 
-			con.query(sqlC, function(err, resultC, fields) {
+		con.query(sqlCS, function(err, resultCS, fields) {
+			if (err) throw err;
+			classSchedule = resultCS;
+			updateSchedule(classSchedule);
+		});
+
+		con.query(sqlC, function(err, resultC, fields) {
+			if (err) throw err;
+			classes = resultC;
+
+			con.query(sqlA, function(err, resultA, fields) {
 				if (err) throw err;
-				const classes = resultC;
-
-				con.query(sqlA, function(err, resultA, fields) {
-					if (err) throw err;
-					const assignments = resultA;
-
-					updateMessage(classes, assignments);
-					updateBot(configs);
-					con.end();
-				});
+				assignments = resultA;
+				updateMessage(classes, assignments);
 			});
+
 		});
 	});
 }
@@ -110,7 +125,7 @@ async function updateBot(configs) {
 }
 
 async function updateMessage(classes, assignments) {
-	var embedString = '{"color": ' + Math.floor(Math.random() * 16777215) + ', "footer": {"text": "Updated: ' + moment().format('MMM-Do h:mma') + '  •  BAIST TODO: v' + pjson.version + '"}, "fields": []}';
+	var embedString = embedHeader;
 
 	var embedObj = JSON.parse(embedString);
 
@@ -287,10 +302,69 @@ async function updateMessage(classes, assignments) {
 		.catch(console.error);
 }
 
+function updateSchedule(schedule) {
+
+	var weekDays = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday'];
+
+	var embedString = embedHeader;
+
+	var embedObj = JSON.parse(embedString);
+
+	if (showShedule == 'on') {
+
+		embedObj['fields'].push({
+			name: '> :clock2:  **Weekly Schedule**  :clock2: ',
+			value: '\u200B',
+		});
+
+		Object.keys(weekDays).forEach(function(keyWDays) {
+			const rowWDays = weekDays[keyWDays];
+			var scheduleString = '\u200B';
+			Object.keys(schedule).forEach(function(keySch) {
+				const rowSch = schedule[keySch];
+				if (rowSch.weekDay.toLowerCase() == rowWDays.toLowerCase()) {
+
+					//console.log(rowSch.weekDay.toLowerCase() + ' ' + rowWDays.toLowerCase());
+
+					var start = moment(rowSch.startTime, 'HH:mm:ss');
+					var end = moment(rowSch.endTime, 'HH:mm:ss');
+					var timeDiff = end.diff(start, 'minutes');
+
+					var startFormat = moment(start).format("h:mmA");
+					var endFromat = moment(end).format("h:mmA");
+
+					var nextClassDay = moment().weekday(1);
+					var nextClassDayDiff = nextClassDay.diff(moment(), 'hours');
+
+					scheduleString += '- ' + rowSch.classID + ' (' + rowSch.location + ') `' + startFormat + ' - ' + endFromat + ' (' + timeDiff + ' mins.)`\n';
+				}
+
+			});
+
+			scheduleString += '\u200B';
+
+			embedObj['fields'].push({
+				name: rowWDays,
+				value: scheduleString,
+			});
+		});
+	}
+
+
+	embedString = JSON.stringify(embedObj);
+
+	const embed = embedObj;
+	const channel = client.channels.cache.get('885537563052490812');
+
+	channel.messages.fetch('886758910118223912')
+		.then(msg => msg.edit('', { embed }))
+		.catch(console.error);
+}
+
 client.once('ready', () => {
 	console.log('Ready!');
-	//const channel = client.channels.cache.get('885537638155710555');
-	//channel.send('beep boop task list goes here');
+	//const channel = client.channels.cache.get('885537563052490812');
+	//channel.send('beep boop');
 	loop();
 });
 
